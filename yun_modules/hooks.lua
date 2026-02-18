17d2584fc93db1fbbf6cc31128e6140a4a926537
yunwulian <799842795@qq.com>
2026-02-18 21:56:23 +0800

    fix a bug

--- yun_modules/hooks.lua
+++ yun_modules/hooks.lua
@@ -125,7 +125,7 @@ end
 
 local pre_hook_enemy_damage = function(args)
     local from = sdk.to_managed_object(args[4]):get_AttackObject()
-    if not from or not from:isMasterPlayer() then return end
+    if from == nil or from:get_type_definition() ~= sdk.find_type_definition("snow.player.PlayerBase") or not from:isMasterPlayer() then return end
     effects.hook_pre_enemy_damage(args)
 end
 
